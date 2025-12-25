e0c92e0086d3fea652d8bfe7c1dabd0a67304dcb
l0c4lxid <syaidxandhika@gmail.com>
2025-12-25 17:21:46 +0700

    trysomething

---
+++ src/app/components/mobile/MobileBackground.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function MobileBackground() {
+  return (
+    <div className="mobile-bg" aria-hidden="true">
+      <div className="mobile-bg__grid" />
+      <div className="mobile-noise" />
+    </div>
+  );
+}
